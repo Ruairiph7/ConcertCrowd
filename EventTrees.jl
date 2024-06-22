e4c146c10cc9e2b2709db98fc2c5ee7773848092
Ruairi Phelan <phelanruairi@gmail.com>
2024-06-22 21:11:17 +0100

    Changed EventTree data structure to combine "people" and "\deltat" into one struct of type "EventData"

--- EventTrees.jl
+++ EventTrees.jl
@@ -4,33 +4,73 @@ using AbstractTrees
 #Binary Search Tree for the event calendar--------------------
 ##############################################################
 
+struct EventData{T1<:Int,T2<:Real}
+    details::AbstractVector{T1}
+    δt::T2
+end #struct
+EventData(details,δt) = EventData{eltype(details),typeof(δt)}(details,δt)
+
 maybeNothing{T} = Union{T,Nothing}
 mutable struct EventTree{T1<:Int,T2<:Real}
-    people::AbstractVector{T1}
-    δt::T2
+    eventData::EventData{T1,T2}
     leftBranch::maybeNothing{EventTree}
     rightBranch::maybeNothing{EventTree}
 
-    function EventTree{T1,T2}(people, δt, l=nothing, r=nothing) where {T1<:Int, T2<:Real}
-        new{T1,T2}(people, δt, l, r)
+    function EventTree{T1,T2}(eventData, l=nothing, r=nothing) where {T1<:Int, T2<:Real}
+        new{T1,T2}(eventData, l, r)
     end #function
 end #struct
-EventTree(people, δt) = EventTree{eltype(people),typeof(δt)}(people, δt)
+EventTree(eventData::EventData) = EventTree{eltype(eventData.details),typeof(eventData.δt)}(eventData)
+EventTree(details::AbstractVector{<:Real}, δt::Real) = EventTree(EventData(details,δt))
 EventTree() = EventTree([0],Inf)
 
-function addToTree!(node::maybeNothing{EventTree}, people::AbstractVector{<:Int}, δt::Real)
-    if δt < node.δt
+function isLessThan(event1::EventData, event2::EventData)
+    if event1.δt < event2.δt
+        return true
+    elseif event1.δt > event2.δt
+        return false
+    elseif event1.δt == event2.δt
+        if event1.details[1] < event2.details[1]
+            return true
+        elseif event1.details[1] > event2.details[1]
+            return false
+        elseif event1.details[1] == event2.details[1]
+            return false
+        end #if
+    end #if
+end #function
+
+function isMoreThan(event1::EventData, event2::EventData)
+    if event1.δt > event2.δt
+        return true
+    elseif event1.δt < event2.δt
+        return false
+    elseif event1.δt == event2.δt
+        if event1.details[1] > event2.details[1]
+            return true
+        elseif event1.details[1] < event2.details[1]
+            return false
+        elseif event1.details[1] == event2.details[1]
+            return false
+        end #if
+    end #if
+end #function
+
+function addToTree!(node::maybeNothing{EventTree}, eventData::EventData)
+    if isLessThan(eventData, node.eventData)
         if isnothing(node.leftBranch)
-            node.leftBranch = EventTree(people, δt)
+            node.leftBranch = EventTree(eventData)
         else
-            addToTree!(node.leftBranch, people, δt)
+            addToTree!(node.leftBranch, eventData)
         end #if
-    elseif δt >= node.δt
+    elseif isMoreThan(eventData, node.eventData)
         if isnothing(node.rightBranch)
-            node.rightBranch = EventTree(people, δt)
+            node.rightBranch = EventTree(eventData)
         else 
-            addToTree!(node.rightBranch, people, δt)
+            addToTree!(node.rightBranch, eventData)
         end #if
+    else
+        error("Adding an 'identical' event --> should not be possible")
     end #if
     return nothing
 end #function
@@ -40,21 +80,21 @@ function getTreeMin(node::EventTree)
     while !isnothing(node.leftBranch)
         node = node.leftBranch
     end #while
-    return node.people, node.δt
+    return node.eventData
 end #function
 
 
-function deleteNode(node::maybeNothing{EventTree}, δtToBeDeleted::Real)
+function deleteNode(node::maybeNothing{EventTree}, dataToBeDeleted::EventData)
     #Return nothing if node given is nothing 
-    #(Needed for the case that δtToBeDeleted is not in the tree)
+    #(Needed for the case that dataToBeDeleted is not in the tree)
     if isnothing(node)
         return nothing
     end #if
     #Move down the tree in the right direction to find δtToBeDeleted
-    if node.δt < δtToBeDeleted
-        node.rightBranch = deleteNode(node.rightBranch, δtToBeDeleted)
-    elseif node.δt > δtToBeDeleted
-        node.leftBranch = deleteNode(node.leftBranch, δtToBeDeleted)
+    if isLessThan(node.eventData, dataToBeDeleted)
+        node.rightBranch = deleteNode(node.rightBranch, dataToBeDeleted)
+    elseif isMoreThan(node.eventData, dataToBeDeleted)
+        node.leftBranch = deleteNode(node.leftBranch, dataToBeDeleted)
     #We have found δtToBeDeleted
     else
         #Node has no children
@@ -69,16 +109,16 @@ function deleteNode(node::maybeNothing{EventTree}, δtToBeDeleted::Real)
         #Node has both left and right children
         else
             rightMin = getTreeMin(node.rightBranch)
-            node = deleteNode(node, rightMin[2])
-            (node.people, node.δt) = rightMin
+            node = deleteNode(node, rightMin)
+            node.eventData = rightMin
             return node
         end #if
     end #if
     return node
 end #function
 
-function deleteNode!(node::maybeNothing{EventTree}, δtToBeDeleted::Real)
-    node = deleteNode(node,δtToBeDeleted)
+function deleteNode!(node::maybeNothing{EventTree}, dataToBeDeleted::EventData)
+    node = deleteNode(node,dataToBeDeleted)
     return nothing
 end #function
 
@@ -99,7 +139,7 @@ function AbstractTrees.children(node::EventTree)
     end #if
 end #function
 
-AbstractTrees.nodevalue(n::EventTree) = (n.δt)
+AbstractTrees.nodevalue(n::EventTree) = (n.eventData.details,n.eventData.δt)
 
 AbstractTrees.NodeType(::Type{<:EventTree{T1,T2}}) where {T1<:Int,T2<:Real} = HasNodeType()
 AbstractTrees.nodetype(::Type{<:EventTree{T1,T2}}) where {T1<:Int,T2<:Real} = EventTree{T1,T2}
